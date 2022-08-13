6cabe1ca64889b5458024cdca561adf887abd159
KY2001 <yukiyoshikein@gmail.com>
2022-08-13 21:54:04 +0900

    first commit

--- AtCoderMySubmissions/abc131/abc131_a.py
+++
@@ -1,8 +0,0 @@
-A=str(input () )
-B=0
-while B =!2:
-if A[B] == A[B+1]:
-    print ('Bad')
-    B += 1 
-    break  
-    print('Good')
--- AtCoderMySubmissions/abc132/abc132_a.py
+++
@@ -1,7 +0,0 @@
-S = input()
-A = set (S)
-B = len (A)
-if B == 2:
-    print ('Yes')
-else:
-    print ('No')
